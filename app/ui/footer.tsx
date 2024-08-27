414985484b05e58b9d9ca48c3464be13c6d5636a
M-Ainurridho <m.ainurridho11@gmail.com>
2024-08-27 15:24:12 +0700

    mobile ui v1 - footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
    return (
-      <footer className="px-32 py-4 bg-neutral-900">
+      <footer className="px-8 py-6 md:px-16 lg:px-32 bg-neutral-900">
          <p className="text-center italic">Copyright &copy; 2024. By Muhammad Ainurridho</p>
       </footer>
    );
