c64c8bd6c24be2d47f527c6655a069d402f998cc
M-Ainurridho <m.ainurridho11@gmail.com>
2024-08-27 15:05:27 +0700

    mobile ui v1 - hero

--- app/ui/home/hero.tsx
+++ app/ui/home/hero.tsx
@@ -4,16 +4,17 @@ import Image from "next/image";
 
 const Hero = () => {
    return (
-      <section id="hero" className="px-32 py-6 flex justify-between items-center grow">
-         <div className="hero-intro">
-            <div>
-               <p className="text-2xl mb-1 flex items-center">
+      <section id="hero" className="px-8 py-6 md:px-16 lg:px-32 flex flex-col md:flex-row justify-between items-center gap-y-8 md:grow">
+         <div className="hero-intro order-2 md:order-1">
+            <div className="text-center md:text-start">
+               <p className="text-xl lg:text-2xl mb-1 hidden md:flex md:items-center">
                   <span>HELLO</span>
                   <span className="inline-block w-16 h-[1px] bg-white ms-2"></span>
                </p>
-               <h1 className="text-5xl mb-1">
+               <h1 className="text-3xl md:text-4xl lg:text-5xl mb-1">
                   <strong>
-                     I&apos;m <span className="inline-block text-green-400">Muhammad Ainurridho</span>
+                     <span className="hidden me-2 md:inline-block">I&apos;m</span>
+                     <span className="inline-block text-green-400">Muhammad Ainurridho</span>
                   </strong>
                </h1>
                <p>Web Developer & Student</p>
@@ -28,9 +29,9 @@ const Hero = () => {
 
             <SocialMedia />
          </div>
-         <div style={{ perspective: "100px" }}>
+         <div className="order-1 md:order-2" style={{ perspective: "100px" }}>
             <div
-               className="w-[350px] h-[350px] bg-neutral-900 rounded-3xl overflow-hidden"
+               className="w-72 h-72 lg:w-[350px] lg:h-[350px] bg-neutral-900 rounded-3xl overflow-hidden"
                style={{
                   boxShadow: "5px 5px 3px #4ade80",
                   transform: "rotateY(-1.5deg)",
--- app/ui/home/social-media.tsx
+++ app/ui/home/social-media.tsx
@@ -27,9 +27,9 @@ const SocialMedia = () => {
       },
    ];
    return (
-      <div className="socmed flex gap-x-4">
+      <div className="flex gap-x-4 justify-center md:justify-start">
          {platforms.map((platform: Platform) => (
-            <Link key={platform.name}  href={platform.url} className="h-8 w-8 border border-green-400 rounded-md flex items-center justify-center hover:shadow-md hover:shadow-green-400 duration-100" target="_blank">
+            <Link key={platform.name} href={platform.url} className="h-8 w-8 border border-green-400 rounded-md flex items-center justify-center hover:shadow-md hover:shadow-green-400 duration-100" target="_blank">
                <FontAwesomeIcon icon={platform.icon} className="h-5 text-green-400" />
             </Link>
          ))}
